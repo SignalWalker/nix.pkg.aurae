5dd7f1ad52ac854e64e354333975c445e4955f85
Ash Walker <ashurstwalker@gmail.com>
2022-11-22 15:37:15 -0500

    style: nix fmt

--- flake.nix
+++ flake.nix
@@ -10,52 +10,54 @@
       url = github:nix-community/naersk;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-	aurae = {
-		url = "github:aurae-runtime/aurae";
-		flake = false;
-	};
+    aurae = {
+      url = "github:aurae-runtime/aurae";
+      flake = false;
+    };
   };
   outputs = inputs @ {
     self,
     nixpkgs,
-	naersk,
-	aurae,
+    naersk,
+    aurae,
     ...
   }:
     with builtins; let
       std = nixpkgs.lib;
-	  systems = [ "x86_64-linux" "aarch64-linux" ];
+      systems = ["x86_64-linux" "aarch64-linux"];
     in {
       formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
-	  overlays = std.genAttrs systems (system: final: prev: let
-	  	nlib = naersk.lib.${system};
-		# vendorDir = src: cranelib.vendorCargoDeps { cargoLock = "${aurae}/Cargo.lock"; inherit src; };
-	  in {
-	  	auraed = nlib.buildPackage {
-			pname = "auraed";
-			src = aurae;
-			targets = [ "aurae" ];
-			cargoBuildOptions = base: base ++ [ "-p" "auraed" ];
-			nativeBuildInputs = with final; [ pkgconf ];
-			buildInputs = with final; [ protobuf libseccomp dbus ];
-		};
-		auraescript = nlib.buildPackage {
-			pname = "auraescript";
-			src = aurae;
-			targets = [ "auraescript" ];
-			cargoBuildOptions = base: base ++ [ "-p" "auraescript" ];
-			nativeBuildInputs = with final; [ pkgconf ];
-			buildInputs = with final; [ protobuf libseccomp dbus ];
-		};
-	  });
-	  packages = mapAttrs (system: overlay: let
-	  	pkgs = import nixpkgs {
-			localSystem = builtins.currentSystem or system;
-			crossSystem = system;
-			overlays = [ overlay ];
-		};
-	  in {
-	  	inherit (pkgs) auraed auraescript;
-	  }) self.overlays;
+      overlays = std.genAttrs systems (system: final: prev: let
+        nlib = naersk.lib.${system};
+        # vendorDir = src: cranelib.vendorCargoDeps { cargoLock = "${aurae}/Cargo.lock"; inherit src; };
+      in {
+        auraed = nlib.buildPackage {
+          pname = "auraed";
+          src = aurae;
+          targets = ["aurae"];
+          cargoBuildOptions = base: base ++ ["-p" "auraed"];
+          nativeBuildInputs = with final; [pkgconf];
+          buildInputs = with final; [protobuf libseccomp dbus];
+        };
+        auraescript = nlib.buildPackage {
+          pname = "auraescript";
+          src = aurae;
+          targets = ["auraescript"];
+          cargoBuildOptions = base: base ++ ["-p" "auraescript"];
+          nativeBuildInputs = with final; [pkgconf];
+          buildInputs = with final; [protobuf libseccomp dbus];
+        };
+      });
+      packages =
+        mapAttrs (system: overlay: let
+          pkgs = import nixpkgs {
+            localSystem = builtins.currentSystem or system;
+            crossSystem = system;
+            overlays = [overlay];
+          };
+        in {
+          inherit (pkgs) auraed auraescript;
+        })
+        self.overlays;
     };
 }
